4fdb6ec9e35b3b9b61315d80747092dee3e86579
finhoose <60304230+finhoose@users.noreply.github.com>
2021-06-30 19:20:12 -0400

    Add files via upload

---
+++ noose.tech.bat
@@ -0,0 +1,696 @@
+:: This file is example of CMD running text for your welcome screen & another project
+:: Copyright (C) 9-10-2018  IRM666 Lab
+:: Visit http://www.youtube.com/channel/UC6Lm1hsWBUgUP_6Jk7uf6yw for more information & example project
+
+
+echo off
+title NOOSE.TECH
+mode 41, 20
+
+:running
+color a
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            **
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            ***
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            ****
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *****
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            ******
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            ********
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *********
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            **********
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            ***********
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            ************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            **************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            ***************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            ****************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *****************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            ******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                              *
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                             **
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                            ***
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                           ****
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                          *****
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                         ******
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                        *******
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                       ********
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                      *********
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                     **********
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                    ***********
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                   ************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                  *************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                 **************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo                ***************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo               ****************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo              *****************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo             ******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo                              *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo                              *
+echo            *                 *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo                              *
+echo            *                 *
+echo            *                 *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo            *                 *
+echo            *                 *
+echo            *                 *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo            *                 *
+echo            *    N            *
+echo            *                 *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo            *                 *
+echo            *    NO           *
+echo            *                 *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo            *                 *
+echo            *    NOO          *
+echo            *                 *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo            *                 *
+echo            *    NOOS         *
+echo            *                 *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo            *                 *
+echo            *    NOOSE        *
+echo            *                 *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo            *                 *
+echo            *    NOOSE.       *
+echo            *                 *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo            *                 *
+echo            *    NOOSE.T      *
+echo            *                 *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo            *                 *
+echo            *    NOOSE.TE     *
+echo            *                 *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo            *                 *
+echo            *    NOOSE.TEC    *
+echo            *                 *
+echo            *******************
+ping localhost -n 1 >nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo            *******************
+echo            *                 *
+echo            *    NOOSE.TECH   *
+echo            *                 *
+echo            *******************
+TIMEOUT /T 2 /NOBREAK >nul
+cls
+goto running
